f10779bbeceb2b0e42730cf8e122593a7d1ce45b
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-09 22:45:38 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+    function addTask(description) {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = description;
+      listContainer.appendChild(taskItem);
+    }
+  
+    let inputFields = document.querySelectorAll('.taskInput');
+    let submitButtons = document.querySelectorAll('.submitTask');
+    let listContainer = document.getElementById('taskList');
+  
+    submitButtons.forEach((button, index) => {
+      button.addEventListener('click', function () {
+        const taskDescription = inputFields[index].value.trim();
+  
+        if (taskDescription !== '') {
+          addTask(taskDescription);
+          inputFields[index].value = '';
+        }
+      });
+    });
+  });
+  
+  
